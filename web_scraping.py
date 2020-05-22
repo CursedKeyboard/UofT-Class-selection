86ff950820d05a2d598ef009ab3d10a2c2d4abf0
Saiem Irfan <Saiem.irfan.2000@gmail.com>
2020-05-22 01:02:58 -0400

    New Module For Organization and More
    Added a new module web_scraping.py for organization. This is going to
    become the new web_scraper.py while that will become a main of sorts.
    
    - Moved old function find_table_course
    
    In addition to this change, the ability to get information regarding a
    course was also added in this module. This new OOP is necessary for
    futureproofing and increasing the ability to play around with the code.
    
    - Added create_course
    - Added function to create a dictionary which helps redirect to right
      page for course information

---
+++ web_scraping.py
@@ -0,0 +1,61 @@
+import requests
+from bs4 import BeautifulSoup, element
+from course import Course
+from typing import Optional
+
+
+def create_subject_to_department_number_dict() -> dict:
+    """ Create a dictionary which maps subjects to department numbers """
+    final_dict = {}
+    with open('Data/subject_to_dep_num.txt', 'r') as file:
+        for line in file:
+            line = line.split(',')
+            subject = line[0]
+            final_dict[subject] = [num.strip() for num in line[1:]]
+
+    return final_dict
+
+
+SUBJECT_TO_DEPARTMENT_NUMBER = create_subject_to_department_number_dict()
+
+
+def find_table_course(program_code: str) -> element.ResultSet:
+    """ Return the bs4 tag which holds all html info about courses """
+    default_gateway = 'http://student.utm.utoronto.ca/calendar/program_detail.pl'
+    program_query = {'Program': program_code}
+    program_page_source = requests.get(default_gateway, params=program_query).text
+    soup = BeautifulSoup(program_page_source, 'lxml')
+
+    table = soup.find('table', class_='tab_adm')
+    return table.find_all(width='80%')
+
+
+def get_program_description(program_code: str) -> str:
+    """ Returns the official program description """
+    with open("program_to_description.csv", 'r') as file:
+        for line in file:
+            if line[:8] == program_code:
+                return line[9:]
+
+    raise ValueError("Program Code not found!")
+
+
+def create_course(course_code: str) -> Optional[Course]:
+    base_url = "http://student.utm.utoronto.ca/calendar/course_detail.pl"
+
+    for department_numbers in SUBJECT_TO_DEPARTMENT_NUMBER[course_code[:3]]:
+        query = {"Depart": str(department_numbers), 'Course': course_code}
+
+        course_page_source = requests.get(base_url, params=query).text
+        soup = BeautifulSoup(course_page_source, 'lxml')
+
+        description = soup.find('span', class_="normaltext")
+
+        if description.b is None:
+            title = soup.find('div', class_='centralpos').p.text[9:]
+            return Course(course_code, description.text, title)
+
+
+if __name__ == '__main__':
+    c = create_course('CSC148H5')
+    print(c.get_desc())
