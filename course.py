aed046ba88d0b9c49cc648e4c07f1b56854109a9
Saiem Irfan <Saiem.irfan.2000@gmail.com>
2020-05-22 14:47:16 -0400

    Finalized Course and Program Classes
    This will likely be the final update regarding these 2 classes. They
    have all the methods they need to hold all relevant info.
    
    - Added _type attribute to Course
    - Changed get_course_type to get _type
    This was previously returning the subject which the course represented
    rather than what type of class it was. This has been changed.
    
    - Added get_course_subject to Course
    This is clearer for use within the rest of the project

--- course.py
+++ course.py
@@ -1,28 +1,87 @@
+from typing import List
+
 
 class Course:
-    """
-    Public Variables:
+    """ An object representation of a UofT course
 
+    === Representation Invariants ===
+    _code:
+        Follows UofT course code standard
     """
-    
-    def __init__(self, name: str, description: str) -> None:
-        """
-        Precondition: Name follows UofT standard
-        """
-        self.name = name
-        self.description = description
-        self.credits = self.credit_count()
+    # === Private Attributes ===
+    # code:
+    #     Course code
+    # description:
+    #     Description for this course
+    # name:
+    #     Name for this course
 
-    def _get_credit_count(self) -> float:
-        """ Return credit count of this course """
-        if self.name[-2].lower() == 'h':
+    def __init__(self, code: str, description: str, name: str, class_type: str) -> None:
+        """
+        Precondition: <code> follows UofT standard
+        """
+        self._code = code
+        self._description = str(description)
+        self._name = name
+        self._type = class_type
+
+    def __str__(self):
+        return "{0}|{1}".format(self._code, self._name)
+
+    def get_credit_count(self) -> float:
+        """ Returns number of credits gained upon successful completion of this course """
+        if self._code[-2].lower() == 'h':
             return 0.5
         else:
             return 1.0
+
+    def get_course_code(self) -> str:
+        """ Returns course code for this course """
+        return self._code
+
+    def get_desc(self) -> str:
+        """ Returns description for this course """
+        return self._description
+
+    def set_desc(self, new_description: str) -> None:
+        """ Modifies description to <new_description> """
+        self._description = new_description
+
+    def get_course_subject(self) -> str:
+        """ Returns the subject type based on UofT code """
+        return self._code[:3]
+
+    def get_course_name(self) -> str:
+        """ Returns the course name """
+        return self._name
+
+    def get_course_type(self):
+        """ Returns the course type """
+        return self._type
+
+
 class Program:
 
-    def __init__(self) -> None:
-        self.courses = list()
-        self.credits = 0.0
+    def __init__(self, name: str, description: str) -> None:
+
+        self._name = str(name)
+        self._description = str(description)
+        self._courses = list()
+
+    def get_name(self) -> str:
+        return self._name
 
     def add_course(self, course: Course) -> bool:
+        if isinstance(course, Course):
+            self._courses.append(course)
+        else:
+            raise TypeError("Argument <course> must be a Course object!")
+
+    def get_courses(self) -> List[Course]:
+        return self._courses
+
+    def get_credit_count(self) -> float:
+        return sum(course for course in self._courses)
+
+    def change_description(self, new_description: str) -> None:
+        self._description = str(new_description)
